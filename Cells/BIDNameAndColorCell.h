c78a46702043ecdab7caac8d147c2af12596d942
David Stroud <david@greymatter-ag.com>
2013-10-18 09:22:44 -0400

    added UITableViewCell class

---
+++ Cells/BIDNameAndColorCell.h
@@ -0,0 +1,13 @@
+//
+//  BIDNameAndColorCell.h
+//  Cells
+//
+//  Created by DAVID STROUD on 10/18/13.
+//  Copyright (c) 2013 Grey Matter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDNameAndColorCell : UITableViewCell
+
+@end
---
+++ Cells/BIDNameAndColorCell.m
@@ -0,0 +1,29 @@
+//
+//  BIDNameAndColorCell.m
+//  Cells
+//
+//  Created by DAVID STROUD on 10/18/13.
+//  Copyright (c) 2013 Grey Matter. All rights reserved.
+//
+
+#import "BIDNameAndColorCell.h"
+
+@implementation BIDNameAndColorCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
